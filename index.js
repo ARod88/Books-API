4949c8c48ce73972df39550ff9d57464755c342a
ARod88 <alma.rodriguez72@yahoo.com>
2023-05-14 00:40:45 -0700

    added a book controller and book models that has schema

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-const express = require('express')
+
+// dependencies
+
+const express = require('express');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+
+// config
+require("dotenv").config();
+const PORT = process.env.PORT;
+console.log("My port is:", PORT);
 
 const app = express();
+
+app.use(methodOverride('_method'));
+
+// mongoose
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true,}, () => {
+    console.log('connected to mongoDB:', process.env.MONGO_URI)
+});
+
+
+
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log("connected to mongoDB: ", process.env.MONGO_URI);
+});
+
 
 // home page
 
@@ -12,7 +37,8 @@ app.get('/', (req, res) => {
 
 // app listener
 
-app.listen(4000, () => {
-    console.log('server started on port 4000');
-});
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running onn port ${process.env.PORT}`);
+    
 
+})
---
+++ models/books.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose 
+
+const bookSchema = new Schema ({
+    title: { type: String },
+    description: { type: String },
+    year: { type: Number },
+    quantity: { type: Number },
+    imageUrl: { type: String}
+})
+
+// model and export
+
+const Book = mongoose.model('Book', bookSchema)
+
+module.exports = Book
